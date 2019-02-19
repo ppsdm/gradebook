bf3966c29c97900a59b1a635a9ef1e8bc53e138d
Samsul Arifin <arifinsam.sam@gmail.com>
2019-02-19 15:55:14 +0700

    initial pdf

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
+use Spipu\Html2Pdf\Html2Pdf;
 
 class ReportController extends Controller
 {
@@ -51,5 +52,36 @@ class ReportController extends Controller
             ['reportReguler' => $output['data']],
             ['userProfile' => $userProfile['data']]
         );
+    }
+
+    public function pdf($userId, $courseId) {
+        $client = new Client;
+        $response = $client->get('http://rest.ppsdm.com:5000/getResult/'. $userId .  '/' . $courseId);
+        $reportReguler =  json_decode($response->getBody(), true);
+
+        $profile = $client->get('http://rest.ppsdm.com:5000/getUserProfile/' . $userId);
+        $userProfile = json_decode($profile->getBody(), true);
+        
+        $uraianNonBase = [];
+        foreach($reportReguler['data']['papi']['uraian_4'] as $data) {
+            $uraianNonBase[] = $data;
+        }
+
+        foreach($reportReguler['data']['papi']['uraian_6'] as $data) {
+            $uraianNonBase[] = $data;
+        }
+
+        foreach($reportReguler['data']['papi']['uraian_8'] as $data) {
+            $uraianNonBase[] = $data;
+        }
+
+        foreach($reportReguler['data']['papi']['uraian_10'] as $data) {
+            $uraianNonBase[] = $data;
+        }
+
+        $htmlpdf = new Html2Pdf('P', 'A4', 'en');
+        $htmlpdf->pdf->SetTitle('title pdf');
+        $htmlpdf->writeHTML(view('result_pdf', compact('reportReguler', 'uraianNonBase', 'userProfile')));
+        $htmlpdf->output('my_doc.pdf');
     }
 }
