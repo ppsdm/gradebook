6363a002163b6d34b91fe54e01b2892e37565ab8
root <root@ip-172-31-20-129.ap-southeast-1.compute.internal>
2019-03-08 07:46:16 +0000

    check user

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ $router->get('/', function () use ($router) {
 });
 
 $router->get('report/{userId}/{courseId}', 'ReportController@index');
+$router->get('checkuser/{userId}/{courseId}', 'ReportController@checkuser');
 $router->get('report-tpa', 'ReportTpaController@index');
 $router->get('lk/{id}/{courseId}', 'ReportLKController@show');
 <<<<<<< 9f65633e6ac6ad8cfccce24d223747145fcd4c90
